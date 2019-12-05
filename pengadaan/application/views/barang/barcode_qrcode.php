d3059194a9541f0d6e3d19c42d5163f80beea845
KevinNatanaelN <knatan55@gmail.com>
2019-12-05 14:40:33 +0700

    web gudang

---
+++ pengadaan/application/views/barang/barcode_qrcode.php
@@ -0,0 +1,48 @@
+<?= $this->session->flashdata('pesan'); ?>
+<div class="card shadow-sm border-bottom-primary">
+    <div class="card-header bg-white py-3">
+        <div class="row">
+            <div class="col">
+                <h4 class="h5 align-middle m-0 font-weight-bold text-primary">
+                    Barcode
+                </h4>
+            </div>
+
+            <div class="box-body">
+            <?php
+            $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
+            echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($barang->id_barang , $generator::TYPE_CODE_128)) . '">';
+                ?>
+                <br>
+            </div>
+            </div>
+        </div>
+    </div>
+   
+
+
+   <div class="card shadow-sm border-bottom-primary">
+    <div class="card-header bg-white py-3">
+        <div class="row">
+            <div class="col">
+                <h4 class="h5 align-middle m-0 font-weight-bold text-primary">
+                   QRCODE
+                </h4>
+            </div>
+
+            <div class="box-body">
+            <?php
+             $qrCode = new Endroid\QrCode\QrCode('Life is too short to be generating QR codes');
+             $qrCode->writeFile( 'uploads/qrcode/item-'.$row->id_barang.'.png');
+                ?>
+                <img src="">
+                <br>
+            </div>
+            </div>
+        </div>
+    </div>
+                
+            </tbody>
+        </table>
+    </div>
+</div>
